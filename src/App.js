3c171ee5940a77ebfb7349008bc0751d79ac9567
streetlightkids <ltj_hq@hotmail.com>
2022-04-13 15:36:38 -0700

    Create App layout
    -Remove default code and styling from create-react-app
    -Add new css
    -Mood object constructor
    -{Mood} state handling
    -Create App layout
    -Pass [moods] and setAllMoments() to Moods()

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Moods from "./components/Moods";
+import Data from "./components/Data";
+import Head from "./components/Head";
+import "./App.css";
 
-function App() {
+export default function App() {
+  //Mood object constructor
+  function Mood(id, color) {
+    this.id = id;
+    this.color = color;
+    this.totalMomentsDuringYear = 0;
+    this.dates = [];
+  }
+
+  //Mood state handling
+  const [down, setDown] = React.useState(new Mood("down", "lightblue"));
+  const [anxious, setAnxious] = React.useState(new Mood("anxious", "yellow"));
+  const [neutral, setNeutral] = React.useState(
+    new Mood("neutral", "peachpuff")
+  );
+  const [confused, setConfused] = React.useState(
+    new Mood("confused", "orange")
+  );
+  const [sexy, setSexy] = React.useState(new Mood("sexy", "pink"));
+  const [angry, setAngry] = React.useState(new Mood("angry", "grey"));
+  const [up, setUp] = React.useState(new Mood("up", "palegreen"));
+  const [fun, setFun] = React.useState(new Mood("fun", "violet"));
+
+  //All moments for the year
+  const [allMoments, setAllMoments] = React.useState(0);
+
+  const moods = [down, anxious, neutral, confused, sexy, angry, up, fun];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Head />
+      <Moods
+        moods={moods}
+        setAllMoments={() => setAllMoments(allMoments + 1)}
+      />
+      <Data />
     </div>
   );
 }
-
-export default App;
