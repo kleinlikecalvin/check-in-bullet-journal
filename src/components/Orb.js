2b1faa5e16d38c54fdb92c75e67d1adf494de7c3
streetlightkids <ltj_hq@hotmail.com>
2022-04-15 10:16:30 -0700

    Create Orb()

--- src/components/Orb.js
+++ src/components/Orb.js
@@ -1,9 +1,21 @@
 import React from "react";
 import "./Orb.css";
+import "./down.css";
+import "./anxious.css";
+import "./confused.css";
+import "./sexy.css";
+import "./angry.css";
+import "./up.css";
+import "./fun.css";
 
-export default function Orb({ color, children }) {
+export default function Orb({ color, children, id, onClick }) {
   return (
-    <button className="Orb" style={{ "--orbMainColor": color }}>
+    <button
+      className="Orb"
+      style={{ "--orbMainColor": color }}
+      id={id}
+      onClick={onClick}
+    >
       <span>{children}</span>
     </button>
   );
