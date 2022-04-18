e7e69ae52a8af17b57d67a35fb708de4502b4f41
streetlightkids <ltj_hq@hotmail.com>
2022-04-18 15:45:39 -0700

    Update Moods.js
    -Depending on the month of the current date use a switch to push the date object into a month array

--- src/components/Moods.js
+++ src/components/Moods.js
@@ -9,7 +9,47 @@ export default function Moods({ moods, setAllMoments }) {
         color={mood.color}
         id={mood.id}
         onClick={() => {
-          mood.dates.push(new Date());
+          const now = new Date();
+          switch (now.getMonth()) {
+            case 0:
+              mood.currentYear.jan.push(now);
+              break;
+            case 1:
+              mood.currentYear.feb.push(now);
+              break;
+            case 2:
+              mood.currentYear.mar.push(now);
+              break;
+            case 3:
+              mood.currentYear.apr.push(now);
+              break;
+            case 4:
+              mood.currentYear.may.push(now);
+              break;
+            case 5:
+              mood.currentYear.jun.push(now);
+              break;
+            case 6:
+              mood.currentYear.jul.push(now);
+              break;
+            case 7:
+              mood.currentYear.aug.push(now);
+              break;
+            case 8:
+              mood.currentYear.sep.push(now);
+              break;
+            case 9:
+              mood.currentYear.oct.push(now);
+              break;
+            case 10:
+              mood.currentYear.nov.push(now);
+              break;
+            case 11:
+              mood.currentYear.dec.push(now);
+              break;
+            default:
+              console.log("I can't figure out where to put this");
+          }
           mood.totalMomentsDuringYear++;
           setAllMoments();
         }}
