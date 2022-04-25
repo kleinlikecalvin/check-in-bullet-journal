202ee5bdb847c304bab5f58f25b9337651281abe
streetlightkids <ltj_hq@hotmail.com>
2022-04-25 12:01:54 -0700

    Update test case and layout

--- src/components/DayVisual.js
+++ src/components/DayVisual.js
@@ -25,45 +25,6 @@ export default function DayVisual({ moods }) {
   const thisMonth = "Apr";
   moodTest.currentYear.apr = [
     new Date(
-      `Fri ${thisMonth} 01 2022 01:42:20 GMT-0700 (Pacific Daylight Time)`
-    ),
-    new Date(
-      `Fri  ${thisMonth} 02 2022 02:42:20 GMT-0700 (Pacific Daylight Time)`
-    ),
-    new Date(
-      `Fri  ${thisMonth} 05 2022 03:42:20 GMT-0700 (Pacific Daylight Time)`
-    ),
-    new Date(
-      `Fri  ${thisMonth} 07 2022 04:42:20 GMT-0700 (Pacific Daylight Time)`
-    ),
-    new Date(
-      `Fri  ${thisMonth} 09 2022 05:42:20 GMT-0700 (Pacific Daylight Time)`
-    ),
-    new Date(
-      `Fri  ${thisMonth} 11 2022 06:42:20 GMT-0700 (Pacific Daylight Time)`
-    ),
-    new Date(
-      `Fri  ${thisMonth} 13 2022 07:42:20 GMT-0700 (Pacific Daylight Time)`
-    ),
-    new Date(
-      `Fri  ${thisMonth} 15 2022 08:42:20 GMT-0700 (Pacific Daylight Time)`
-    ),
-    new Date(
-      `Fri  ${thisMonth} 17 2022 09:42:20 GMT-0700 (Pacific Daylight Time)`
-    ),
-    new Date(
-      `Fri  ${thisMonth} 18 2022 10:42:20 GMT-0700 (Pacific Daylight Time)`
-    ),
-    new Date(
-      `Fri  ${thisMonth} 18 2022 11:42:20 GMT-0700 (Pacific Daylight Time)`
-    ),
-    new Date(
-      `Fri  ${thisMonth} 19 2022 12:42:20 GMT-0700 (Pacific Daylight Time)`
-    ),
-    new Date(
-      `Fri  ${thisMonth} 20 2022 13:42:20 GMT-0700 (Pacific Daylight Time)`
-    ),
-    new Date(
       `Fri  ${thisMonth} 21 2022 14:42:20 GMT-0700 (Pacific Daylight Time)`
     ),
     new Date(
@@ -78,8 +39,11 @@ export default function DayVisual({ moods }) {
     new Date(),
   ];
   return (
-    <table className="DayVisual">
-      <DayVisualRow moods={moods} test={moodTest} />
-    </table>
+    <div className="DayVisual">
+      <h2>Day</h2>{" "}
+      <table className="DayVisual">
+        <DayVisualRow moods={moods} test={moodTest} />
+      </table>
+    </div>
   );
 }
