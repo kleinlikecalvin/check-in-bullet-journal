f33985f3ca82f49d49255bbe688c535885502c09
streetlightkids <ltj_hq@hotmail.com>
2022-04-25 12:03:43 -0700

    Update styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Moods from "./components/Moods";
-import Data from "./components/Data";
+import VisualDisplay from "./components/VisualDisplay";
 import Head from "./components/Head";
 import "./App.css";
 
@@ -8,7 +8,7 @@ export default function App() {
   //Mood object constructor
   function Mood(id, color) {
     this.id = id;
-    this.color = color;
+    this.color = `#${color}`;
     this.totalMomentsDuringYear = 0;
     this.currentYear = {
       jan: [],
@@ -26,27 +26,27 @@ export default function App() {
     };
   }
 
-  //Mood state handling
-  const [down, setDown] = React.useState(new Mood("down", "lightblue"));
-  const [anxious, setAnxious] = React.useState(new Mood("anxious", "yellow"));
-  const [neutral, setNeutral] = React.useState(
-    new Mood("neutral", "peachpuff")
-  );
+  //Moods state handling
+  const [down, setDown] = React.useState(new Mood("down", "A3C4F3"));
+  const [anxious, setAnxious] = React.useState(new Mood("anxious", "FBF8CC"));
+  const [neutral, setNeutral] = React.useState(new Mood("neutral", "FDE4CF"));
   const [confused, setConfused] = React.useState(
-    new Mood("confused", "orange")
+    new Mood("confused", "FFCFD2")
   );
-  const [sexy, setSexy] = React.useState(new Mood("sexy", "pink"));
-  const [angry, setAngry] = React.useState(new Mood("angry", "grey"));
-  const [up, setUp] = React.useState(new Mood("up", "palegreen"));
-  const [fun, setFun] = React.useState(new Mood("fun", "violet"));
+  const [sexy, setSexy] = React.useState(new Mood("sexy", "F1C0E8"));
+  const [angry, setAngry] = React.useState(new Mood("angry", "D2D2CF"));
+  const [up, setUp] = React.useState(new Mood("up", "B9FBC0"));
+  const [fun, setFun] = React.useState(new Mood("fun", "CFBAF0"));
+
+  const moods = [up, down, fun, anxious, neutral, confused, sexy, angry];
 
   //All moments for the year
   const [allMoments, setAllMoments] = React.useState(0);
-  const moods = [up, down, fun, anxious, neutral, confused, sexy, angry];
+
   return (
     <div className="App">
-      <Data allMoments={allMoments} moods={moods} />
-      <Head />
+      <Head allMoments={allMoments} moods={moods} />
+      <VisualDisplay moods={moods} />
       <Moods
         moods={moods}
         setAllMoments={() => setAllMoments(allMoments + 1)}
