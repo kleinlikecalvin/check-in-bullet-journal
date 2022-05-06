be0a28a20071a193abd8606d4af3b531d299e3e9
streetlightkids <ltj_hq@hotmail.com>
2022-05-06 11:55:17 -0700

    Replaced Head with TextDisplay
    The code for Head was so minimal (it only connected to TextDisplay with one class in CSS) so I decided to replace it with the code from TextDisplay and update whatever was necessary

--- src/components/Head.js
+++ src/components/Head.js
@@ -1,11 +1,60 @@
 import React from "react";
-import TextDisplay from "./TextDisplay";
+import DaysRemaining from "./DaysRemaining";
 import "./Head.css";
 
 export default function Head({ allMoments, moods }) {
+  const moodCount = moods.map((mood) => {
+    if (mood.totalMomentsDuringYear === 1) {
+      return (
+        <li key={mood.id}>
+          I had{" "}
+          <span
+            style={{
+              backgroundColor: mood.color,
+            }}
+          >
+            {mood.totalMomentsDuringYear}
+          </span>{" "}
+          moment that I was feeling{" "}
+          <span
+            style={{
+              backgroundColor: mood.color,
+            }}
+          >
+            {mood.id}
+          </span>{" "}
+          this year.
+        </li>
+      );
+    } else {
+      return (
+        <li key={mood.id}>
+          I had{" "}
+          <span
+            style={{
+              backgroundColor: mood.color,
+            }}
+          >
+            {mood.totalMomentsDuringYear}
+          </span>{" "}
+          moments that I was feeling{" "}
+          <span
+            style={{
+              backgroundColor: mood.color,
+            }}
+          >
+            {mood.id}
+          </span>{" "}
+          this year.
+        </li>
+      );
+    }
+  });
   return (
-    <div className="Head">
-      <TextDisplay allMoments={allMoments} moods={moods} />
-    </div>
+    <ul className="Head">
+      <li>Total moments recorded this year : {allMoments.allMomentsTally}</li>
+      <DaysRemaining />
+      {moodCount}
+    </ul>
   );
 }
